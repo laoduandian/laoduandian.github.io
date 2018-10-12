d6a236a349f90e1c5052d2387959be5a2921e651
laoduandian <laoduandian@sina.com>
2018-10-12 11:40:39 +0800

    a

---
+++ data.js
@@ -0,0 +1,8 @@
+var ks=[
+  {name:"狂点白块儿",name_en:"Music Tiles2",img:"/games/musictiles-ad.jpg",iosid:1175680497},
+  {name:"骑士对决",name_en:"Duel Of Clans",img:"/games/clans-ad.jpg",iosid:375561039},
+  {name:"英跳跃钢琴键之白块全模式",name_en:"Spring Piano Tiles",img:"games/springpianotiles-ad.jpg",iosid:1006380436},
+  {name:"小孩快跑",name_en:"Boy Go Go Go",img:"/games/boygogogo-ad.jpg",iosid:995785137},
+  {name:"钢琴砖块",name_en:"The Piano Block",img:"/games/thepianoblock-ad.jpg",iosid:1032777274},
+  {name:"圈来圈去",name_en:"Spring Circle Run",img:"/games/springcirclerun-ad.jpg",iosid:1013757451}
+];
