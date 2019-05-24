f5910de03e332e51a7eb5cd6fe513433499839f9
laoduandian <laoduandian@sina.com>
2019-05-24 17:44:13 +0800

    1

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var ks=[
-  {name:"狂点白块儿",name_en:"Music Tiles2",img:"/games/musictiles-ad.jpg",iosid:1175680497,page:"com.ccy.game.pianotiles.nnn",show:"true",inland:"www.baidu.com"},
-  {name:"骑士对决",name_en:"Duel Of Clans",img:"/games/clans-ad.jpg",iosid:1375561039,page:"com.lotus.game.tafangnumberone",show:"true",inland:"www.baidu.com"},
-  {name:"英跳跃钢琴键之白块全模式",name_en:"Spring Piano Tiles",img:"games/springpianotiles-ad.jpg",iosid:1006380436,page:"com.shenweiyi.game.piano.tiles",show:"true",inland:"www.baidu.com"},
-  {name:"小孩快跑",name_en:"Boy Go Go Go",img:"/games/boygogogo-ad.jpg",iosid:995785137,page:"com.lotus.game.boygogo.android",show:"true",inland:"www.baidu.com"},
-  {name:"钢琴砖块",name_en:"The Piano Block",img:"/games/thepianoblock-ad.jpg",iosid:1032777274,page:"com.lts.game.musicblock",show:"false",inland:"www.baidu.com"},
-  {name:"圈来圈去",name_en:"Spring Circle Run",img:"/games/springcirclerun-ad.jpg",iosid:1013757451,page:"com.lotus.game.circlerun",show:"true",inland:"www.baidu.com"}
+  {name:"狂点白块儿",name_en:"Music Tiles2",img:"/games/musictiles-ad.jpg",iosid:1175680497,page:"com.ccy.game.pianotiles.nnn",show:"true",inland:"https://www.baidu.com"},
+  {name:"骑士对决",name_en:"Duel Of Clans",img:"/games/clans-ad.jpg",iosid:1375561039,page:"com.lotus.game.tafangnumberone",show:"true",inland:"https://www.baidu.com"},
+  {name:"英跳跃钢琴键之白块全模式",name_en:"Spring Piano Tiles",img:"games/springpianotiles-ad.jpg",iosid:1006380436,page:"com.shenweiyi.game.piano.tiles",show:"true",inland:"https://www.baidu.com"},
+  {name:"小孩快跑",name_en:"Boy Go Go Go",img:"/games/boygogogo-ad.jpg",iosid:995785137,page:"com.lotus.game.boygogo.android",show:"true",inland:"https://www.baidu.com"},
+  {name:"钢琴砖块",name_en:"The Piano Block",img:"/games/thepianoblock-ad.jpg",iosid:1032777274,page:"com.lts.game.musicblock",show:"false",inland:"https://www.baidu.com"},
+  {name:"圈来圈去",name_en:"Spring Circle Run",img:"/games/springcirclerun-ad.jpg",iosid:1013757451,page:"com.lotus.game.circlerun",show:"true",inland:"https://www.baidu.com"}
 ];
